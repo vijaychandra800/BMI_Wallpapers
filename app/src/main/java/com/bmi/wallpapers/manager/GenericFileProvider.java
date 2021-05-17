cd8466962e94b09a5d01d06a2b4ac913deeac807
vijaychandra800 <vijaychandra800@gmail.com>
2021-05-17 15:19:47 +0530

    Changed Packgaed Name

---
+++ app/src/main/java/com/bmi/wallpapers/manager/GenericFileProvider.java
@@ -0,0 +1,11 @@
+package com.bmi.wallpapers.manager;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by Tamim on 05/11/2017.
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+}
---
+++ app/src/main/java/com/bmi/wallpapers/shaderprograms/shaderutil/LoggerConfig.java
@@ -0,0 +1,9 @@
+package com.bmi.wallpapers.shaderprograms.shaderutil;
+
+/**
+ * Created by MartinRGB on 2017/2/26.
+ */
+
+public class LoggerConfig {
+    public static final boolean ON = true;
+}
